35267d0a9d00d6be69a2de7fc8f7257bba4cb986
Renato <renato.postigo@ucsp.edu.pe>
2020-04-17 19:59:41 -0500

    Player lives, destroying non-seen cubes

--- Unity3D/Assets/Cubes.cs
+++ Unity3D/Assets/Cubes.cs
@@ -3,16 +3,22 @@ using System.Collections;
 
 public class Cubes : MonoBehaviour {
 
-	private float delay = 0.8f;
+	private float delay = 1.1f; //in seconds
+	private float MIN_DEPTH = -5.5f;
 	public GameObject cube;
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("Spawn", delay, delay);
+		InvokeRepeating ("Spawn", delay, 0.0f);
 	}
-	
-	// Update is called once per frame
+
 	void Spawn () {
 		Instantiate (cube, new Vector3 (Random.Range (-6, 6), 10, 0), Quaternion.identity);
+	}
+
+	void Update(){
+		if (cube.transform.position.y <= MIN_DEPTH){
+			Destroy(cube);
+		}
 	}
 }
---
+++ Unity3D/Assets/Sphere.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+	public int lives = 5;
+
+	void Start(){
+		print("Start Sphere");
+	}
+
+	void Update(){
+		
+	}
+
+    private void OnCollisionEnter(Collision collision){
+    	this.lives--;
+    	print("lives: " + this.lives);
+    	//Destroy(collision.gameObject);
+
+    	if (this.lives == 0){
+    		Destroy(gameObject, 2);
+    	}
+    }
+
+    void OnGUI(){
+		Rect rectObj= new Rect (40,450,200,400);
+		
+		GUIStyle style = new GUIStyle ();
+		
+		style.alignment = TextAnchor.UpperLeft;
+		
+		if (this.lives > 0){
+			GUI.Box (rectObj,"Lives remaining: " + this.lives, 
+		          style );
+		} else {
+			GUI.Box (rectObj,"GAME OVER", 
+		          style );
+		}
+
+	}
+
+}
